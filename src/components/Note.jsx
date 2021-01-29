74bd63c975aa1d7ac7dd6765b34bd4c5515a3fd6
unknown <kevin.cheung.kai@gmail.com>
2021-01-28 23:51:51 -0500

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Note() {
+    return(
+        <div className='note'>
+            <h1>First Note</h1>
+            <p>Content for note</p>
+        </div>
+    )
+}
+
+export default Note;
