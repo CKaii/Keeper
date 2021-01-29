1df55280170ef8bedffed3e4f7108e5e31f0c7d0
unknown <kevin.cheung.kai@gmail.com>
2021-01-29 00:07:52 -0500

    Added notes file and mapped notes to update dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Note from './components/Note'
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+import notes from './notes';
 
 function App() {
-    return(
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      {notes.map((noteContent) => {
+        return (
+          <Note
+            key={noteContent.key}
+            title={noteContent.title}
+            content={noteContent.content}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
