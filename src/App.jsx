9b277be2df07dd758176db27263567217ca8ebae
unknown <kevin.cheung.kai@gmail.com>
2021-02-01 17:26:37 -0500

    Added delete button to note component and added createarea component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
+import CreateArea from './components/CreateArea';
 import notes from './notes';
 
 function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((noteContent) => {
         return (
           <Note
